623735ca4ccdb2980d00b68bf88f44cd2f612648
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-08-23 11:48:56 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.eva1_1_hola_mundo;
+/**
+ *
+ * @author mario
+ */
+public class EVA1_1_HOLA_MUNDO {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!");
+    }
+}
