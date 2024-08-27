63e7a258a652f3ec1a6658031c111dce9ed02410
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-08-27 16:13:38 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                                           // Tipos de datos en java
+        System.out.println("HOLA MUNDO!!");// Cadena de texto
+        System.out.println(100);     // Un numero entero
+        System.out.println('c');     // Un caracter
+        System.out.println(true);    //valores logicos
+        System.out.println(false);
+        System.out.println(1000.52); //números con fracción (con punto decimal)
+    
+        //Tipos de datos
+        //Enteros
+        //int
+        //long
+        //byte
+        //Short
+//Punto flotante(numeros decimales)
+        //double
+        //float
+//Texto
+        //String
+        //caracter
+        //char
+//LOGICOS (true, False)
+        //boleean
+        
+//Creacion de una varaiable
+        //Declaracion - tipo de dato de la variable; que operaciones debe o puede hacer; Cuanta memora va a utilizar
+       int edad; //Declaro una variable de tipo entero llama edad
+                 //edad
+//       double edad; // NO PODEMOS TENER 2 VARIABLES CON EL MISMO NOMBRE
+        //Asignacion - Poner un valor a la variabe; nombre de la variable = valor;
+        String nombre;
+        String apellidos;
+        double promedio;
+        
+        edad = 18;// estoy asignando el valor 18 dentro de la variable edad
+        System.out.println(edad);// println tambien imprime la variable 
+    }
+    
+}
