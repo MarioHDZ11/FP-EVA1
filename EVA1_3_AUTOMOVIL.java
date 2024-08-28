5b33085444a065b5fb3ac092759bb54192d32911
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-08-27 21:06:40 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_automovil;
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_AUTOMOVIL {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Declaracion de las variabes
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        // Asignar valores
+        marca = "FORD";
+        modelo = "MUSTANG";
+        year = 2022;
+        precio = 99000.1;
+        
+        System.out.println(marca);// Improime el contenido de la variable marca
+        System.out.println(modelo);//
+        System.out.println(year);//
+        System.out.println(precio);//
+    }
+    
+}
