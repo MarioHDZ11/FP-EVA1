e8d48419d325e28c264eaa59befa58be287ef4fb
MarioHDZ11 <L24550357@chihuahua2.tecnm.mx>
2024-08-27 21:06:59 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_television;
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TELEVISION {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String tecnologia;
+        int pulgadas;
+        double precio;
+        
+        marca = "samsung";
+        tecnologia = "OLED";
+        pulgadas = 24;
+        precio = 4299.99;
+        
+        System.out.println(marca);
+        System.out.println(tecnologia);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        
+    }
+    
+}
